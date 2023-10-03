552a697c021b67aaa7d6f486d34c260c07fba09d
GhassanAlKaraan <ghass.karaan@gmail.com>
2023-10-03 13:06:36 +0300

    first commit

---
+++ bin/dart_basics.dart
@@ -0,0 +1,5 @@
+import 'package:dart_basics/dart_basics.dart' as dart_basics;
+
+void main(List<String> arguments) {
+  dart_basics.calculate();
+}
---
+++ lib/dart_basics.dart
@@ -0,0 +1,4 @@
+void calculate() {
+  print("Hello world");
+}
+
